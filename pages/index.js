0e28ea0d4ea86edaf7d1463c20ddbc97f4ec3c81
Jirat Ki <jirat.ki@wework.com>
2020-08-25 00:07:52 +0800

    fix: 🐛 Correct title

--- pages/index.js
+++ pages/index.js
@@ -10,10 +10,10 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>There is my name and my nickname in my domain name - Jirat.Net</title>
-        <meta name="description" content="Once upon a time... This domain was belong to someone else, but now it is mine!" />
+        <meta name="description" content="Once upon a time. This domain was belong to someone, now it is mine!" />
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta property="og:title" content="Jirat Ki - Net" key="title" />
+        <meta property="og:title" content="There is my name and my nickname in my domain name - Jirat.Net" key="title" />
         <meta property="og:image" content="https://jirat.net/og-image.png" />
       </Head>
       <div className={styles.backgroundContainer}>
